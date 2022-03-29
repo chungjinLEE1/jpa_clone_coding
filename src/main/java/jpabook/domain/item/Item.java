20daaa6e9bcae3674d776f05a239f32098d68b82
chungjinLEE1 <cjlee1@brinks.co.kr>
2022-03-29 16:04:37 +0900

    주문 기능 테스트 완

--- src/main/java/jpabook/domain/item/Item.java
+++ src/main/java/jpabook/domain/item/Item.java
@@ -39,9 +39,11 @@ public abstract class Item {
     public void addStock(int quantity){
         this.stockQuantity += quantity;
     }
+
+
 //  stock 감소
     public void removeStock(int quantity){
-        int restStock = this.stockQuantity -= quantity;
+        int restStock = this.stockQuantity - quantity;
         if(restStock < 0){
             throw new NotEnoughStockException("need more stock");
         }
